c7f6d76048b60ad83e91e43bba88f5084329a822
sawal612 <sawalsharma63@gmail.com>
2026-06-06 21:02:31 +0530

    fix case sensitivity

--- Backend/controllers/workoutcontroller.js
+++ Backend/controllers/workoutcontroller.js
@@ -1,4 +1,4 @@
-const workout = require("../models/workoutmodel");
+const workout = require("../models/workoutModel");
 // importing the workoutModel to interact with the workouts collection in the database
 
 const mongoose = require('mongoose')
